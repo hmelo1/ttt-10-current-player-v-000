75b47daa28a9ca0bd8c0dc1ceae3bf7c98f46b7f
Eddie Melo <heriberto@melo.nyc>
2017-11-20 15:11:27 +0000

    automatically backed up by learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -1,8 +1,12 @@
 def turn_count(board)
-
+  turn_count = 0
+  board.each do |turn|
+    if (turn == "X" || turn == "O")
+      turn_count += 1
+    end
+  end
 end
 
 def current_player()
 
 end
-  
