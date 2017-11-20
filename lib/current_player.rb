ae9f0e8af91db44844db2a3f482412f54c7ac84b
Eddie Melo <heriberto@melo.nyc>
2017-11-20 15:24:43 +0000

    automatically backed up by learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -9,10 +9,10 @@ def turn_count(board)
 end
 
 def current_player(board)
-  if (turn_count(board).even?)
-    "X"
-  else
-    "O"
-  end
+  #if (turn_count(board).even?)
+    #"X"
+  #else
+    #"O"
+  #end
   turn_count(board).even? ? "X" : "O"
 end
